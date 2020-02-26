7011c7d30088b848eb5a6a1373c1180feafed102
erick-ite <a01630105@itesm.mx>
2020-02-26 03:07:11 +0000

    práctica 2

---
+++ xv6-public/reboot.c
@@ -0,0 +1,49 @@
+#include "types.h"
+
+
+
+#include "stat.h"
+
+
+
+#include "user.h"
+
+
+
+
+
+
+
+
+
+
+
+int
+
+
+
+main(int argc, char *argv[])
+
+
+
+{
+
+
+
+
+
+
+
+    printf(1, "me voy a reiniciar... %s\n");
+
+
+
+    reboot();
+
+
+
+    exit();
+
+
+
+}
